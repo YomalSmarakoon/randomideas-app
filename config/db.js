1b7a9b7850a6cc5aa6b680f50fb2237991d5d552
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-15 17:32:03 +0530

    Mongoose Schema & Model & Queries.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-// mongoose.connect(process.env.PORT);
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
+
+// mongoose.set('strictQuery', true)
 
 module.exports = connectDB;
