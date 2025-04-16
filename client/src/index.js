4adebd1ddc268677ca9b77a2ca1828e4303f8d8a
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-16 15:34:06 +0530

    Deployement changes.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import IdeaFrom from "./components/IdeaForm";
 import IdeaList from "./components/IdeaList";
 import Modal from "./components/Modal";
 import "./css/style.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
 
 new Modal();
 
