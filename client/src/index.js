ae2723f554cd90e023ca017422fb7db8be5fda3b
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-16 09:38:03 +0530

    Modal component - Before.

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,24 @@
-import './css/style.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+import "./css/style.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+const modalBtnEl = document.getElementById("modal-btn");
+const modalEl = document.getElementById("modal");
+
+// let modalOpen = false;
+
+function onOpenModal() {
+  modalEl.style.display = "block";
+}
+
+function onCloseModal() {
+  modalEl.style.display = "none";
+}
+
+function outSideClick(e) {
+  if (e.target === modalEl) {
+    onCloseModal();
+  }
+}
+
+modalBtnEl.addEventListener("click", onOpenModal);
+window.addEventListener("click", outSideClick);
