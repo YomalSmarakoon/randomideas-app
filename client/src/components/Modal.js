e60c9ebd05aa4612bda4e9e7ecfa8dcb4ca1f17d
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-16 10:40:52 +0530

    Modal component - After.

---
+++ client/src/components/Modal.js
@@ -0,0 +1,28 @@
+class Modal {
+  constructor() {
+    this._modalBtnEl = document.getElementById("modal-btn");
+    this._modalEl = document.getElementById("modal");
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this._modalBtnEl.addEventListener("click", this.onOpenModal.bind(this));
+    window.addEventListener("click", this.outSideClick.bind(this));
+  }
+
+  onOpenModal() {
+    this._modalEl.style.display = "block";
+  }
+
+  onCloseModal() {
+    this._modalEl.style.display = "none";
+  }
+
+  outSideClick(e) {
+    if (e.target === this._modalEl) {
+      this.onCloseModal();
+    }
+  }
+}
+
+export default Modal;
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,5 @@
+import Modal from "./components/Modal";
 import "./css/style.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-const modalBtnEl = document.getElementById("modal-btn");
-const modalEl = document.getElementById("modal");
-
-// let modalOpen = false;
-
-function onOpenModal() {
-  modalEl.style.display = "block";
-}
-
-function onCloseModal() {
-  modalEl.style.display = "none";
-}
-
-function outSideClick(e) {
-  if (e.target === modalEl) {
-    onCloseModal();
-  }
-}
-
-modalBtnEl.addEventListener("click", onOpenModal);
-window.addEventListener("click", outSideClick);
+const modal = new Modal();
