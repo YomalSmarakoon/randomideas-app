b4a985dcd9bcd0e56d426727ef9963a518a42827
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-16 09:14:05 +0530

    client folder setup.

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const bodyParser = require('body-parser')
-require('dotenv').config();
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const path = require("path");
+
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const connectDB = require('./config/db')
 
 connectDB();
 const app = express();
 
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // body parser middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   //   res.send("Hello World");
   res.json({ message: "Welcome to the RandomIdeas API" });
 });
 
-const ideasRouter = require('./routes/ideas')
-app.use('/api/ideas', ideasRouter);
+const ideasRouter = require("./routes/ideas");
+app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
